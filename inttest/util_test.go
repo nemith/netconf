66c1d7652113beee4016798fed4dc60d328494a8
Brandon Bennett <brandon@brbe.me>
2022-10-24 16:06:41 -0600

    inttest: Add Arista cEOS
    This adds arista cEOS as a integration test (and also fixes a bug for
    upgrading to chunked transport)

--- inttest/util_test.go
+++ inttest/util_test.go
@@ -2,6 +2,7 @@ package inttest
 
 import (
 	"bytes"
+	"strconv"
 	"testing"
 )
 
@@ -19,25 +20,6 @@ func newLogWriter(prefix string, t *testing.T) *logWriter {
 }
 
 func (w *logWriter) Write(p []byte) (int, error) {
-	for _, ch := range p {
-		switch ch {
-		case '\r':
-			// skip
-		case '\n':
-			w.t.Log(w.prefix, w.buf.String())
-			w.buf.Reset()
-		default:
-			w.buf.WriteByte(ch)
-		}
-
-	}
+	w.t.Log(w.prefix, strconv.Quote(string(p)))
 	return len(p), nil
-}
-
-func (w *logWriter) Close() error {
-	// flush the rest of the buffer to a log
-	if w.buf.Len() > 0 {
-		w.t.Log(w.prefix, w.buf.String())
-	}
-	return nil
 }
